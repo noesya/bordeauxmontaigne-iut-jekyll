1ce3529fc3a6dd83669f3456f7aff3c65f1bb7ee
Arnaud Levy <contact@arnaudlevy.com>
2021-10-27 14:41:32 +0200

    nav

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem 'jekyll'
+gem 'nokogiri'
 
 # Responsive images
 # https://github.com/rbuchberger/jekyll_picture_tag
---
+++ _plugins/page_nav_filter.rb
@@ -0,0 +1,27 @@
+require 'nokogiri'
+module Jekyll::PageNavFilter
+  def page_nav_links(html)
+    doc = Nokogiri::HTML html
+    nav = "<ul>"
+    doc.css('h2').each_with_index do |node, index|
+      id = make_id index
+      nav += "<li><a href=\"##{id}\">#{node.content}</a></li>"
+    end
+    nav += "</ul>"
+    nav
+  end
+
+  def page_nav_text_with_ids(html)
+    doc = Nokogiri::HTML html
+    doc.css('h2').each_with_index do |node, index|
+      node['id'] = make_id index
+    end
+    doc.to_s
+  end
+
+  def make_id(index)
+    "title-#{index+1}"
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::PageNavFilter)
